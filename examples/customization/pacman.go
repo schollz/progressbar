828f3bf730310b9f292385c265a0cc1f7abe6280
Wystan Schmidt <wystans@gmail.com>
2021-11-29 18:49:24 -0700

    Added basic pacman customization

---
+++ examples/customization/pacman.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
+)
+
+func main() {
+	doneCh := make(chan struct{})
+
+	bar := progressbar.NewOptions(1000,
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionSetWidth(50),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        " ",
+			SaucerHead:    "[yellow]<[reset]",
+			SaucerPadding: "[white]•",
+			BarStart:      "[blue]|[reset]",
+			BarEnd:        "[blue]|[reset]",
+		}),
+		progressbar.OptionOnCompletion(func() {
+			doneCh <- struct{}{}
+		}),
+	)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			bar.Add(1)
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	// got notified that progress bar is complete.
+	<-doneCh
+	fmt.Println("\n ======= progress bar completed ==========\n")
+}
